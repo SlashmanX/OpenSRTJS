451985935117a18bb02a9668c98fa8dbaf8cfd13
emartin <emartin@transperfect.com>
2014-04-29 17:11:13 +0100

    Added funcion to get OpenSRT ID from the URL

--- opensrt.js
+++ opensrt.js
@@ -1,4 +1,7 @@
 var xmlrpc = require("xmlrpc");
+var zlib = require("zlib");
+var request = require("request");
+var cheerio = require("cheerio");
 
 var USER_AGENT = "OS Test User Agent";
 
@@ -6,7 +9,7 @@ var client = xmlrpc.createClient({ host: 'api.opensubtitles.org', port: 80, path
 
 var token = "";
 
-exports.getToken = function(cb) {
+exports.getToken = function getToken(cb) {
 	client.methodCall('LogIn', ['', '', 'en', USER_AGENT], function (err, res) {
 		if(err) return cb(err, null);
 		cb(null, res.token);
@@ -14,9 +17,41 @@ exports.getToken = function(cb) {
 },
 
 exports.getSRTs = function(data, cb) {
-	if(!data.token) return cb("Token not supplied", null);
+	if(!data.token) {
+		exports.getToken(function(err, token) {
+			data.token = token;
+			getSRT(data, cb);
+		});
+	}
+
+	else {
+		getSRT(data, cb);
+	}
+}
+
+exports.getSRTFromURL = function(data, cb) {
+	request({uri: "http://www.opensubtitles.org/en/subtitles/"+ data.subs, followAllRedirects: true}, function(err, res, body) {
+		if(err || !body) return cb(err, null);
+		var $ = cheerio.load(body);
+		var id = $("a.none").filter(function() {
+			var link = $(this);
+			return link.attr("href").match(/\/en\/subtitleserve\/file\/(.*)/);
+		});
+		id = id.attr("href").replace("/en/subtitleserve/file/", "");
+		exports.getSRTs({subs: [id]}, function(err, srt) {
+			if(err) return cb(err, null);
+			return cb(null, srt);
+		})
+	})
+}
+
+function getSRT(data, cb) {
 	client.methodCall('DownloadSubtitles', [data.token, data.subs], function (err, res) {
 		if(err) return cb(err, null);
-		cb(null, res);
+		var buffer = new Buffer(res.data[0].data, "base64");
+		zlib.unzip(buffer, function(err, data){
+			if(err) cb(err, null);
+			cb(null, data.toString());
+		});
   })
 }
--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 var opensrt = require("./opensrt.js");
 var token = "";
-var btoa = require("btoa");
 var zlib = require("zlib");
 
 opensrt.getToken(function(err, token) {
 	if(err) console.error(err);
 	this.token = token;
 
-	opensrt.getSRTs({token: token, subs: [1951854837]}, function(err, res){
+	opensrt.getSRTs({token: token, subs: [4973341]}, function(err, res){
 		if(err) return console.error(err);
 		var buffer = new Buffer(res.data[0].data, "base64");
 		zlib.unzip(buffer, function(err, data){

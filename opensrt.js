14821722259842a31696612a5a9ac834a2f3de59
emartin <emartin@transperfect.com>
2014-05-02 12:26:08 +0100

    v0.0.1 - returns link to most popular subtitles of each langauge. Search by IMDB, seaosn and Episode

--- opensrt.js
+++ opensrt.js
@@ -1,9 +1,7 @@
 var xmlrpc = require("xmlrpc");
-var zlib = require("zlib");
-var request = require("request");
-var cheerio = require("cheerio");
+var async = require("async");
 
-var USER_AGENT = "OS Test User Agent";
+var USER_AGENT = "OpenSubtitlesPlayer v4.7";
 
 var client = xmlrpc.createClient({ host: 'api.opensubtitles.org', port: 80, path: '/xml-rpc'})
 
@@ -12,46 +10,61 @@ var token = "";
 exports.getToken = function getToken(cb) {
 	client.methodCall('LogIn', ['', '', 'en', USER_AGENT], function (err, res) {
 		if(err) return cb(err, null);
+		token = res.token;
 		cb(null, res.token);
   })
 },
 
-exports.getSRTs = function(data, cb) {
-	if(!data.token) {
+exports.searchEpisode = function(data, cb) {
+	if(!token && !data.token) {
 		exports.getToken(function(err, token) {
+			if(err) {
+				console.log("Token");
+				return cb(err, null);
+			}
 			data.token = token;
-			getSRT(data, cb);
+			searchEpisode(data, cb);
 		});
 	}
 
 	else {
-		getSRT(data, cb);
+		searchEpisode(data, cb);
 	}
 }
 
-exports.getSRTFromURL = function(data, cb) {
-	request({uri: "http://www.opensubtitles.org/en/subtitles/"+ data.subs, followAllRedirects: true}, function(err, res, body) {
-		if(err || !body) return cb(err, null);
-		var $ = cheerio.load(body);
-		var id = $("a.none").filter(function() {
-			var link = $(this);
-			return link.attr("href").match(/\/en\/subtitleserve\/file\/(.*)/);
-		});
-		id = id.attr("href").replace("/en/subtitleserve/file/", "");
-		exports.getSRTs({subs: [id]}, function(err, srt) {
+function searchEpisode(data, cb) {
+	var subs = [];
+	client.methodCall('SearchSubtitles', [
+			data.token, 
+			[
+				{
+					sublanaguageid: "all", 
+					imdbid: data.imdbid.replace("tt", ""), 
+					season: data.season, 
+					episode: data.episode
+				}
+			]
+		], 
+		function(err, res){
 			if(err) return cb(err, null);
-			return cb(null, srt);
-		})
-	})
-}
 
-function getSRT(data, cb) {
-	client.methodCall('DownloadSubtitles', [data.token, data.subs], function (err, res) {
-		if(err) return cb(err, null);
-		var buffer = new Buffer(res.data[0].data, "base64");
-		zlib.unzip(buffer, function(err, data){
-			if(err) cb(err, null);
-			cb(null, data.toString());
-		});
-  })
-}
+			async.eachSeries(res.data, function(sub, callback) {
+				var tmp = {};
+				tmp.srtLink = sub.SubDownloadLink.replace(".gz", ".srt");
+				tmp.lang = sub.ISO639;
+				tmp.downloads = sub.SubDownloadsCnt;
+				if(!subs[tmp.lang]) {
+					subs[tmp.lang] = tmp;
+				}
+				else {
+					if(tmp.downloads > subs[tmp.lang].downloads) { // Most downloaded seems a good metric for best subtitle
+						subs[tmp.lang] = tmp;
+					}
+				}
+				return callback();
+			},
+			function(err, res) {
+				cb(err, subs);
+			})
+		})
+	}
--- test.js
+++ test.js
@@ -2,16 +2,7 @@ var opensrt = require("./opensrt.js");
 var token = "";
 var zlib = require("zlib");
 
-opensrt.getToken(function(err, token) {
-	if(err) console.error(err);
-	this.token = token;
-
-	opensrt.getSRTs({token: token, subs: [4973341]}, function(err, res){
-		if(err) return console.error(err);
-		var buffer = new Buffer(res.data[0].data, "base64");
-		zlib.unzip(buffer, function(err, data){
-			console.log(err);
-			console.log(data.toString());
-		});
-	})
+opensrt.searchEpisode({imdbid: "0121955", season: "1", episode: "1"}, function(err, res){
+	if(err) return console.error("Error: "+ err);
+	console.log(res);
 })
